d9eda496a11e64f7315cf03633a7180ac369c7c2
DuarteSJ <ddduarte@sapo.pt>
2025-10-06 17:37:36 +0200

    added markview and jupynium pluggins to nvf

--- home/terminal/nvf.nix
+++ home/terminal/nvf.nix
@@ -92,6 +92,94 @@
         transparent = true;
       };
 
+      # Add extra plugins
+      extraPlugins = {
+        # Markview for enhanced markdown rendering
+        markview = {
+          package = pkgs.vimPlugins.markview-nvim;
+          setup = ''
+            require('markview').setup({
+              preview = {
+                modes = { "n", "no", "c" },
+                hybrid_modes = { "n" },
+                
+                callbacks = {
+                  on_enable = function (_, win)
+                    vim.wo[win].conceallevel = 2;
+                    vim.wo[win].concealcursor = "c";
+                  end
+                }
+              }
+            })
+          '';
+        };
+        
+        # Jupynium for Jupyter notebook integration
+        jupynium = {
+          package = pkgs.vimUtils.buildVimPlugin {
+            name = "jupynium.nvim";
+            src = pkgs.fetchFromGitHub {
+              owner = "kiyoon";
+              repo = "jupynium.nvim";
+              rev = "master";
+              sha256 = "13ssf2fpikfghmjr39nafjsdr83amddn4m9bqpp443ab852ai6d6";
+            };
+          };
+          setup = ''
+            require('jupynium').setup({
+              python_host = vim.g.python3_host_prog or "python3",
+              default_notebook_URL = "localhost:8888/nbclassic",
+              
+              auto_start_server = {
+                enable = false,
+                file_pattern = { "*.ju.*" },
+              },
+              
+              auto_attach_to_server = {
+                enable = true,
+                file_pattern = { "*.ju.*", "*.md" },
+              },
+              
+              auto_start_sync = {
+                enable = false,
+                file_pattern = { "*.ju.*" },
+              },
+              
+              auto_download_ipynb = true,
+              auto_close_tab = true,
+              
+              autoscroll = {
+                enable = true,
+                mode = "always",
+              },
+              
+              use_default_keybindings = true,
+              
+              syntax_highlight = {
+                enable = true,
+              },
+            })
+            
+            -- Highlight groups for jupynium cells
+            vim.cmd [[
+              hi! link JupyniumCodeCellSeparator CursorLine
+              hi! link JupyniumMarkdownCellSeparator CursorLine
+              hi! link JupyniumMarkdownCellContent CursorLine
+              hi! link JupyniumMagicCommand Keyword
+            ]]
+          '';
+        };
+        
+        # Optional: nvim-notify for better notifications
+        nvim-notify = {
+          package = pkgs.vimPlugins.nvim-notify;
+          setup = ''
+            require('notify').setup()
+            vim.notify = require('notify')
+          '';
+        };
+      };
+
       # Custom keybindings
       keymaps = [
         # Toggle file tree with Ctrl+N
