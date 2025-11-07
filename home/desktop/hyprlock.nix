1f0fc97620e4d935383faf2bc49f57184586d904
DuarteSJ <ddduarte@sapo.pt>
2025-11-07 13:18:53 +0100

    changes to hyprlock

--- home/desktop/hyprlock.nix
+++ home/desktop/hyprlock.nix
@@ -8,18 +8,25 @@ in {
       background = [
         {
           monitor = "";
-          path = "screenshot";
-          blur_passes = 2;
-          contrast = 1.0;
-          brightness = 0.8;
-          vibrancy = 0.2;
-          vibrancy_darkness = 0.2;
+          path = "/home/duartesj/Pictures/lock-screen/snow-mountain.jpg";
+        }
+        {
+          monitor = "";
+          zindex = 1;
+          keep_aspect_ratio = true;
+          rounding = 0;
+          border_size = 0;
+          path = "/home/duartesj/Pictures/lock-screen/snow-mountain-overlay.png";
         }
       ];
+
       # GENERAL
       general = {
-        hide_cursor = false;
+        no_fade_in = true;
+        no_fade_out = true;
+        hide_cursor = true;
         grace = 0;
+        disable_loading_bar = true;
       };
 
       # INPUT FIELD
@@ -28,8 +35,8 @@ in {
           monitor = "";
           size = "250, 60";
           outline_thickness = 2;
-          dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
+          dots_size = 0.2;
+          dots_spacing = 0.35;
           dots_center = true;
           outer_color = "rgba(0, 0, 0, 0)";
           inner_color = "rgba(0, 0, 0, 0.2)";
@@ -42,6 +49,7 @@ in {
           position = "0, -200";
           halign = "center";
           valign = "center";
+          zindex = 1;
         }
       ];
 
@@ -50,22 +58,33 @@ in {
         # DATE
         {
           monitor = "";
-          text = "cmd[update:1000] echo \"$(date +\"%A, %B %d\")\"";
-          color = "rgb(${colors.base05})";
-          font_size = 22;
-          font_family = "JetBrains Mono";
-          position = "0, 300";
+          text = "cmd[update:1000] echo \"$(date +\"%A, %d %b\")\"";
+          color = "rgba(255, 255, 255, 0.9)";
+          font_size = 60;
+          font_family = "Adwaita Sans, thin";
+          position = "0, 375";
           halign = "center";
           valign = "center";
         }
-        # TIME
+        # TIME - HOURS
         {
           monitor = "";
-          text = "cmd[update:1000] echo \"$(date +\"%H:%M\")\"";
-          color = "rgb(${colors.base05})";
-          font_size = 95;
-          font_family = "JetBrains Mono Extrabold";
-          position = "0, 200";
+          text = "cmd[update:1000] echo -e \"$(date +\"%H\")\"";
+          color = "rgba(172, 166, 180, 1)";
+          font_size = 120;
+          font_family = "Adwaita Sans, Heavy";
+          position = "-145, 90";
+          halign = "center";
+          valign = "center";
+        }
+        # TIME - MINUTES
+        {
+          monitor = "";
+          text = "cmd[update:1000] echo -e \"$(date +\"%M\")\"";
+          color = "rgba(255, 255, 255, 1)";
+          font_size = 120;
+          font_family = "Adwaita Sans, Heavy";
+          position = "145, 90";
           halign = "center";
           valign = "center";
         }
