fcec312645e843306722f8dce754403acf7de2ea
DuarteSJ <ddduarte@sapo.pt>
2025-11-08 02:15:49 +0100

    change to hyprland animations

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -66,31 +66,19 @@
       # Animations
       animations = {
         enabled = true;
-
         bezier = [
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-          "almostLinear,0.5,0.5,0.75,1.0"
-          "quick,0.15,0,0.1,1"
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.1, 1.1, 0.1, 1.1"
+          "winOut, 0.3, -0.3, 0, 1"
         ];
-
         animation = [
-          "global, 1, 10, default"
-          "border, 1, 5.39, easeOutQuint"
-          "windows, 1, 4.79, easeOutQuint"
-          "windowsIn, 1, 3.49, easeOutQuint, popin 17%"
-          "windowsOut, 1, 3.49, linear, popin 17%"
-          "fadeIn, 1, 1.73, almostLinear"
-          "fadeOut, 1, 1.46, almostLinear"
-          "fade, 1, 3.03, quick"
-          "layers, 1, 3.81, easeOutQuint"
-          "layersIn, 1, 4, easeOutQuint, fade"
-          "layersOut, 1, 1.5, linear, fade"
-          "fadeLayersIn, 1, 1.79, almostLinear"
-          "fadeLayersOut, 1, 1.39, almostLinear"
-          "workspaces, 1, 3.21, easeInOutCubic, slide"
-          "specialWorkspace, 1, 5, easeInOutCubic, slidevert"
+          "windowsIn, 1, 4, winIn, popin"
+          "windowsOut, 1, 3, winOut, popin"
+          "windowsMove, 1, 3, wind, slide"
+          "border, 1, 6, wind"
+          "fade, 1, 3, wind"
+          "workspaces, 1, 4, wind"
+          "specialWorkspace, 1, 4, wind, slidefadevert 70%"
         ];
       };
 
