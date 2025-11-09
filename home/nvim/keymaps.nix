197456de6280c532f7654c4b87e544ff24221b44
Duarte São José <ddduarte@sapo.pt>
2025-11-09 17:37:26 +0100

    Refactor nvim (#1)
    * refactor: change some nvim keybindings
    
    * refactor: nvim file structure

--- home/nvim/keymaps.nix
+++ home/nvim/keymaps.nix
@@ -1,31 +1,41 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
-  programs.nvf.settings.vim.keymaps = [
-    # Open file explorer with Ctrl+n
-    {
-      mode = "n";
-      key = "<C-n>";
-      action = "<cmd>lua require('mini.files').open()<CR>";
-    }
-    # Close current buffer with Leader+X
-    {
-      mode = "n";
-      key = "<leader>x";
-      action = ":bdelete<CR>";
-    }
-    # Buffer cycling with Tab and Shift+Tab
-    {
-      mode = "n";
-      key = "<Tab>";
-      action = ":bnext<CR>";
-    }
-    {
-      mode = "n";
-      key = "<S-Tab>";
-      action = ":bprevious<CR>";
-    }
-  ];
+  programs.nvf.settings.vim = {
+    keymaps = [
+      {
+        mode = "n";
+        key = "<C-n>";
+        action = "<cmd>lua require('mini.files').open()<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>x";
+        action = ":bdelete<CR>";
+      }
+      {
+        mode = "n";
+        key = "<Tab>";
+        action = ":bnext<CR>";
+      }
+      {
+        mode = "n";
+        key = "<S-Tab>";
+        action = ":bprevious<CR>";
+      }
+      {
+        mode = "i";
+        key = "kj";
+        action = "<Esc>";
+      }
+    ];
+
+    autocmds = [
+      {
+        event = ["TextYankPost"];
+        pattern = ["*"];
+        command = ''
+          lua vim.highlight.on_yank({higroup="IncSearch", timeout=50})
+        '';
+      }
+    ];
+  };
 }
