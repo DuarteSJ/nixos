95482f22dd088a4538a1bf81aaec61ed22015b3c
DuarteSJ <ddduarte@sapo.pt>
2026-01-11 15:57:14 +0000

    change output and build dir for VimTex

--- home/nvim/extra/latex.nix
+++ home/nvim/extra/latex.nix
@@ -6,8 +6,6 @@
         vim.g.vimtex_view_method = "zathura"
         vim.g.vimtex_compiler_method = "latexmk"
         vim.g.vimtex_compiler_latexmk = {
-          aux_dir = ".build",
-          out_dir = ".build",
           continuous = 1,
         }
         vim.g.vimtex_quickfix_mode = 1
