0e735e8cd3423805efe2ad54caaf3e25003a539a
DuarteSJ <ddduarte@sapo.pt>
2026-03-20 14:54:42 +0000

    fix obsidian.nvim warnings

--- home/nvim/extra/notes.nix
+++ home/nvim/extra/notes.nix
@@ -4,7 +4,6 @@ in {
   programs.nvf.settings.vim = {
     notes.obsidian = {
       enable = true;
-
       setupOpts = {
         workspaces = [
           {
@@ -12,19 +11,16 @@ in {
             path = "~/notes";
           }
         ];
-
         completion = {
           nvim_cmp = true;
           min_chars = 2;
         };
-
         daily_notes = {
           folder = "daily";
           date_format = "%d-%m-%Y";
           alias_format = "%B %-d, %Y";
           template = "daily.md";
         };
-
         templates = {
           subdir = "templates";
           date_format = "%d-%m-%Y";
@@ -42,60 +38,37 @@ in {
             '';
           };
         };
-
-        note_id_func = mkLuaInline ''
-          function(title)
-            if title ~= nil then
-              return title
-            end
-            -- If no title provided, use timestamp
-            vim.notify("No title provided, using timestamp", vim.log.levels.WARN)
-            return tostring(os.date("%d%m%Y%H%M%S"))
-          end
-        '';
-
-        note_frontmatter_func = mkLuaInline ''
+        frontmatter.func = mkLuaInline ''
           function(note)
             local out = {
               aliases = note.aliases,
               tags = note.tags,
               created = os.date("%d-%m-%Y %H:%M"),
             }
-
-            -- Preserve any existing metadata
             if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
               for k, v in pairs(note.metadata) do
                 out[k] = v
               end
             end
-
             return out
           end
         '';
-
-        follow_url_func = mkLuaInline ''
-          function(url)
-            vim.fn.jobstart({"xdg-open", url})
-          end
-        '';
-
+        legacy_commands = false;
         finder = "telescope.nvim";
-
         notes_subdir = "zettelkasten";
-
+        checkbox.order = [ " " "d" "f" ];
         ui = {
-          enable = true;
           checkboxes = {
             " " = {
               char = "󰄱";
               hl_group = "ObsidianTodo";
             };
             "d" = {
-              char = "";
+              char = "";
               hl_group = "ObsidianDone";
             };
             "f" = {
-              char = "";
+              char = "";
               hl_group = "ObsidianTilde";
             };
           };
@@ -109,47 +82,44 @@ in {
         };
       };
     };
-
-    # keybindings
     keymaps = [
       {
         mode = "n";
         key = "<leader>on";
-        action = ":ObsidianNew<CR>";
+        action = ":Obsidian new<CR>";
       }
       {
         mode = "n";
         key = "<leader>oo";
-        action = ":ObsidianQuickSwitch<CR>";
+        action = ":Obsidian quick_switch<CR>";
       }
       {
         mode = "n";
         key = "<leader>os";
-        action = ":ObsidianSearch<CR>";
+        action = ":Obsidian search<CR>";
       }
       {
         mode = "n";
         key = "<leader>od";
-        action = ":ObsidianToday<CR>";
+        action = ":Obsidian today<CR>";
       }
       {
         mode = "n";
         key = "<leader>ob";
-        action = ":ObsidianBacklinks<CR>";
+        action = ":Obsidian backlinks<CR>";
       }
       {
         mode = "n";
         key = "<leader>ol";
-        action = ":ObsidianLinks<CR>";
+        action = ":Obsidian links<CR>";
       }
       {
         mode = "n";
         key = "<leader>ot";
-        action = ":ObsidianNewFromTemplate<CR>";
+        action = ":Obsidian new_from_template<CR>";
       }
     ];
   };
-
   home.activation.createObsidianDirs = lib.hm.dag.entryAfter ["writeBoundary"] ''
     $DRY_RUN_CMD mkdir -p $VERBOSE_ARG ~/notes/{templates,daily,zettelkasten}
   '';
